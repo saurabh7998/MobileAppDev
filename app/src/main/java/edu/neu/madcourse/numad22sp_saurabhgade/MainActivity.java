b9911c95982629edddcd52ef2699296fc32f6293
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-01-21 00:30:03 -0500

    adds toast feature

--- app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/MainActivity.java
@@ -2,13 +2,33 @@ package edu.neu.madcourse.numad22sp_saurabhgade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutMeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutMeBtn = findViewById(R.id.button);
+
+        aboutMeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Saurabh, gade.sau@northeastern.edu";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
