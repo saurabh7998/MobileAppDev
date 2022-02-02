cd1bd11e38db1611b72aeb072812bcac9fb45922
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-02-01 20:44:46 -0500

    connected main activity with grid activity using intent

--- app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/GridActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/GridActivity.java
@@ -2,13 +2,35 @@ package edu.neu.madcourse.numad22sp_saurabhgade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GridActivity extends AppCompatActivity {
+
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        Intent intent = getIntent();
+
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnD = findViewById(R.id.btnD);
+        btnE = findViewById(R.id.btnE);
+        btnF = findViewById(R.id.btnF);
+
+
+
     }
 }
