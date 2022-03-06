c9b49ae4781ec3f2eadbf92b18e6674e8867f2ef
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-03-06 18:58:49 -0500

    connects to api and gets response based on user input

--- app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/ApiActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_saurabhgade/ApiActivity.java
@@ -1,39 +1,50 @@
 package edu.neu.madcourse.numad22sp_saurabhgade;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-//import org.json.simple.*;
-//import org.json.simple.parser.*;
 
 
 public class ApiActivity extends AppCompatActivity {
 
     private Button submitBtn;
     private EditText editTextMovie;
-    private TextView responseView1;
+    private TextView genreView;
+    private TextView yearView;
+    private TextView imdbView;
+    private TextView errorView;
+    private RadioButton movieRadioBtn;
+    private RadioButton seriesRadioBtn;
+    private RadioButton epRadioBtn;
+    private ImageView imageView;
     private HttpURLConnection conn;
     private BufferedReader bufferedReader;
+
+    private ProgressDialog progressBar;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +55,15 @@ public class ApiActivity extends AppCompatActivity {
 
         submitBtn = findViewById(R.id.submitMovieBtn);
         editTextMovie = findViewById(R.id.editTextMovieName);
-        responseView1 = findViewById(R.id.responseView1);
+        genreView = findViewById(R.id.genreView);
+        yearView = findViewById(R.id.yearView);
+        imdbView = findViewById(R.id.imdbView);
+        errorView = findViewById(R.id.errorView);
+        movieRadioBtn = findViewById(R.id.movieRadioBtn);
+        seriesRadioBtn = findViewById(R.id.seriesRadioBtn);
+        epRadioBtn = findViewById(R.id.epRadioBtn);
+        imageView = findViewById(R.id.imageView2);
+
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,15 +76,38 @@ public class ApiActivity extends AppCompatActivity {
 
 
     private void connectToApiOnNewThread() {
-        Handler handler = new Handler();
 
+
+        if (editTextMovie.getText() == null || editTextMovie.getText().toString().equals("")) {
+            errorView.setText("Please select valid options for the request");
+            return;
+        }
+        String movieName = editTextMovie.getText().toString();
+
+//        if (!epRadioBtn.isActivated() && !movieRadioBtn.isActivated() && !seriesRadioBtn.isActivated()) {
+//            responseView1.setText("Please select valid options for the request");
+//            return;
+//        }
+        String type = "";
+        if (epRadioBtn.isActivated()) {
+            type = "episode";
+        }
+        if (movieRadioBtn.isActivated()) {
+            type = "movie";
+        }
+        if (seriesRadioBtn.isActivated()) {
+            type = "series";
+        }
+
+
+        String finalType = type;
         Runnable objRunnable = new Runnable() {
             @Override
             public void run() {
                 String line = "";
                 StringBuilder response = new StringBuilder();
                 try {
-                    URL url = new URL("https://www.omdbapi.com/?apikey=497dcded&s=the%20avengers");
+                    URL url = new URL("https://www.omdbapi.com/?apikey=497dcded&t=" + movieName + "&type=" + finalType);
                     conn = (HttpURLConnection) url.openConnection();
 
                     conn.setRequestMethod("GET");
@@ -86,16 +128,38 @@ public class ApiActivity extends AppCompatActivity {
                     }
                     bufferedReader.close();
 
+
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
+
                             System.out.println(response);
+                            String data = response.toString();
+
                             try {
+
                                 JSONObject obj = new JSONObject(response.toString());
-                                JSONArray arr = obj.getJSONArray("Search");
-                                JSONObject arrObj = arr.getJSONObject(0);
-                                String title = arrObj.getString("Title");
-                                System.out.println("Title of movie is " + title);
+                                String title = obj.getString("Title");
+                                String year = obj.getString("Year");
+                                String genre = obj.getString("Genre");
+                                String imdb = obj.getString("imdbRating");
+
+                                genreView.setText("Genre: " + genre);
+                                yearView.setText("Year: " + year);
+                                imdbView.setText("IMDB: " + imdb);
+
+                                String poster = obj.getString("Poster");
+                                new FetchImage(poster).start();
+
+//                                JSONArray arr = obj.getJSONArray("Search");
+//                                JSONObject arrObj = arr.getJSONObject(0);
+//                                String title = arrObj.getString("Title");
+//
+//                                String year = arrObj.getString("Year");
+//                                System.out.println("Title of movie is " + title);
+//                                responseView1.setText(year);
+
+
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -116,44 +180,54 @@ public class ApiActivity extends AppCompatActivity {
 
     }
 
+    class FetchImage extends Thread {
+        String url;
+        Bitmap bitmap;
 
-    private void connectToApi() {
-
-
-        String line = "";
-        StringBuilder response = new StringBuilder();
-
-        try {
-            URL url = new URL("https://www.omdbapi.com/?apikey=497dcded&s=the%20avengers");
-            conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("GET");
-            conn.setConnectTimeout(4000);
-            conn.setReadTimeout(4000);
-
-
-            int status = conn.getResponseCode();
-
-            if (status > 299) {
-                bufferedReader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-            } else {
-                bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            }
-            while ((line = bufferedReader.readLine()) != null) {
-                response.append(line);
-            }
-            bufferedReader.close();
-            System.out.println(response.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            conn.disconnect();
+        FetchImage(String url) {
+            this.url = url;
         }
 
-    }
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    progressBar = new ProgressDialog(ApiActivity.this);
+                    progressBar.setMessage("Loading response");
+                    progressBar.setCancelable(false);
+                    progressBar.show();
 
+                }
+            });
+
+            InputStream inputStream = null;
+            try {
+                inputStream = new URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (progressBar.isShowing()) {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        progressBar.dismiss();
+                    }
+
+                    imageView.setImageBitmap(bitmap);
+                }
+            });
+
+
+        }
+    }
 
 }
 
